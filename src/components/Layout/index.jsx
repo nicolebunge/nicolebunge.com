d5603b6062e944d91505d3e77762e2186b421c1d
Marco Biedermann <marco.biedermann@gmx.de>
2018-09-28 09:26:49 +0200

    refactor: transform Layout component to PureComponent

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,59 +1,69 @@
 import { graphql, Link, StaticQuery } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { PureComponent } from 'react';
 import Helmet from 'react-helmet';
+import 'typeface-source-sans-pro';
 import Grid from '../Grid';
 import Header from '../Header';
-import 'typeface-source-sans-pro';
 import './style.css';
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <div>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <Header>
-          <Grid>
-            <h1 style={{ margin: 0 }}>
-              <Link
-                to="/"
-                style={{
-                  color: 'white',
-                  textDecoration: 'none',
-                }}
-              >
-                {data.site.siteMetadata.title}
-              </Link>
-            </h1>
-          </Grid>
-        </Header>
-        <Grid>
-          {children}
-        </Grid>
-      </div>
-    )}
-  />
-);
+class Layout extends PureComponent {
+  static propTypes = {
+    children: PropTypes.node,
+  };
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+  static defaultProps = {
+    children: null,
+  };
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <StaticQuery
+        query={graphql`
+          query SiteTitleQuery {
+            site {
+              siteMetadata {
+                title
+              }
+            }
+          }
+        `}
+        render={data => (
+          <div>
+            <Helmet
+              title={data.site.siteMetadata.title}
+              meta={[
+                { name: 'description', content: 'Sample' },
+                { name: 'keywords', content: 'sample, something' },
+              ]}
+            >
+              <html lang="en" />
+            </Helmet>
+            <Header>
+              <Grid>
+                <h1 style={{ margin: 0 }}>
+                  <Link
+                    to="/"
+                    style={{
+                      color: 'white',
+                      textDecoration: 'none',
+                    }}
+                  >
+                    {data.site.siteMetadata.title}
+                  </Link>
+                </h1>
+              </Grid>
+            </Header>
+            <Grid>
+              {children}
+            </Grid>
+          </div>
+        )}
+      />
+    );
+  }
+}
 
 export default Layout;
