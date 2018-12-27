1e254f6d1face040b587ed7f9fe408d13bfd8921
Marco Biedermann <marco.biedermann@gmx.de>
2018-12-27 17:05:39 +0100

    feat: Social
    Add social icons

--- src/pages/kontakt.jsx
+++ src/pages/kontakt.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ContactForm from '../components/ContactForm';
 import Layout from '../components/Layout';
 import email from '../images/email.svg';
+import instagram from '../images/instagram.svg';
+import linkedin from '../images/linkedin.svg';
+import xing from '../images/xing.svg';
 
 const ContactPage = () => (
   <Layout>
@@ -16,6 +19,38 @@ const ContactPage = () => (
       <br />
       <img src={email} alt="E-Mail Adresse" width="147.1" height="15" />
     </address>
+    <ul>
+      <li>
+        <a
+          href="https://www.instagram.com/nicole.bunge/"
+          itemProp="sameAs"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={instagram} alt="Nicole Bunge auf Instagram" />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/nicole-bunge-1b891415a/"
+          itemProp="sameAs"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={linkedin} alt="Nicole Bunge auf LinkedIn" />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.xing.com/profile/Nicole_Bunge3"
+          itemProp="sameAs"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={xing} alt="Nicole Bunge auf Xing" />
+        </a>
+      </li>
+    </ul>
     <ContactForm />
   </Layout>
 );
