2dee92c7c6dc6a6732cc902d20f5b98bc07440fe
Marco Biedermann <marco.biedermann@gmx.de>
2019-01-16 23:21:38 +0100

    feat: Social
    add social component

---
+++ src/components/Social/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Icons from '../Icons';
+import styles from './style.module.css';
+
+const Social = () => (
+  <div className={styles.social}>
+    <Icons />
+  </div>
+);
+
+export default Social;
