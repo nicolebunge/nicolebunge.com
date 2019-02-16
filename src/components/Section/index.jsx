dd40a75b769250459e4cf734d43267828e886b2f
Marco Biedermann <marco.biedermann@gmx.de>
2019-02-16 22:27:59 +0100

    feat: Section
    add full size option to section

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
 import styles from './style.module.css';
@@ -5,17 +6,23 @@ import styles from './style.module.css';
 class Section extends PureComponent {
   static propTypes = {
     children: PropTypes.node,
+    full: PropTypes.bool,
   };
 
   static defaultProps = {
     children: null,
+    full: false,
   };
 
   render() {
-    const { children } = this.props;
+    const { children, full } = this.props;
 
     return (
-      <section className={styles.section}>
+      <section
+        className={classNames(styles.section, {
+          [styles.sectionFull]: full,
+        })}
+      >
         {children}
       </section>
     );
