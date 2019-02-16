aa44ab9094acd9ff8f969a3f94866e3990aca037
Marco Biedermann <marco.biedermann@gmx.de>
2019-02-16 22:32:48 +0100

    feat: Form
    update Form component

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Formik, Form, Field } from 'formik';
+import GridColumn from '../GridColumn';
 import Label from '../Label';
+import GridRow from '../GridRow';
 import styles from './style.module.css';
 
 const ContactForm = () => (
@@ -9,46 +11,60 @@ const ContactForm = () => (
       action="/kontakt/danke"
       data-netlify-honeypot="bot-field"
       data-netlify="true"
+      className={styles.form}
       method="post"
       name="contact"
     >
-      <Field
-        name="form-name"
-        type="hidden"
-        value="contact"
-      />
+      <Field name="form-name" type="hidden" value="contact" />
 
-      <div className={styles.form__field}>
-        <Label htmlFor="name">Name</Label>
-        <Field
-          autocomplete="name"
-          className={styles.form__input}
-          name="name"
-          required
-          type="text"
-        />
-      </div>
+      <GridRow>
+        <GridColumn columnSpan={6}>
+          <div className={styles.form__field}>
+            <Field
+              autoComplete="name"
+              className={styles.form__input}
+              name="name"
+              placeholder="Name"
+              required
+              type="text"
+            />
+            <Label className={styles.form__label} htmlFor="name">
+              Name
+            </Label>
+          </div>
+        </GridColumn>
 
-      <div className={styles.form__field}>
-        <Label htmlFor="email">E-Mail</Label>
-        <Field
-          autocomplete="email"
-          className={styles.form__input}
-          name="email"
-          required
-          type="email"
-        />
-      </div>
+        <GridColumn columnSpan={6} columnStart={7}>
+          <div className={styles.form__field}>
+            <Field
+              autoComplete="email"
+              className={styles.form__input}
+              name="email"
+              placeholder="E-Mail"
+              required
+              type="email"
+            />
+            <Label className={styles.form__label} htmlFor="email">
+              E-Mail
+            </Label>
+          </div>
+        </GridColumn>
 
-      <div className={styles.form__field}>
-        <Label htmlFor="message">Nachricht</Label>
-        <Field
-          className={styles.form__input}
-          component="textarea"
-          name="message"
-          required
-        />
-      </div>
+        <GridColumn columnSpan={12}>
+          <div className={styles.form__field}>
+            <Field
+              className={styles.form__input}
+              component="textarea"
+              name="message"
+              placeholder="Nachricht"
+              required
+            />
+            <Label className={styles.form__label} htmlFor="message">
+              Nachricht
+            </Label>
+          </div>
+        </GridColumn>
+      </GridRow>
 
       <div className={styles.form__field}>
         <button type="submit">Senden</button>
