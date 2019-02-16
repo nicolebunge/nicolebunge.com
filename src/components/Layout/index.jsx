2e76e8deb1e09660e8cde42643700443b4d05773
Marco Biedermann <marco.biedermann@gmx.de>
2019-02-16 22:33:14 +0100

    feat: Layout
    update Layout component

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -7,7 +7,7 @@ import Cookie from '../Cookie';
 import Footer from '../Footer';
 import Grid from '../Grid';
 import Header from '../Header';
-import Icons from '../Icons';
+import Social from '../Social';
 import Main from '../Main';
 import Navigation from '../Navigation';
 import * as routes from '../../constants/routes';
@@ -49,57 +49,46 @@ class Layout extends PureComponent {
               <html lang="en" />
             </Helmet>
             <Header>
-              <div
-                style={{
-                  flexGrow: 1,
-                }}
-              >
-                <h1>
-                  <Link to="/">
-                    <img
-                      src={nicoleBunge}
-                      alt={data.site.siteMetadata.title}
-                      width="160"
-                    />
-                  </Link>
-                </h1>
+              <Link to="/">
+                <img
+                  src={nicoleBunge}
+                  alt={data.site.siteMetadata.title}
+                  width="160"
+                />
+              </Link>
+
+              <Navigation
+                routes={[
+                  routes.INDEX,
+                  routes.ABOUT,
+                  routes.ACTING,
+                  routes.THEATRE_PEDAGOGY,
+                  routes.CONTACT,
+                ]}
+              />
+            </Header>
+            <Main>
+              {children}
+            </Main>
+            <Footer>
+              <Grid>
                 <Navigation
                   routes={[
-                    routes.INDEX,
-                    routes.ABOUT,
-                    routes.ACTING,
-                    routes.THEATRE_PEDAGOGY,
-                    routes.CONTACT,
+                    routes.PRIVACY,
+                    routes.IMPRINT,
                   ]}
                 />
-              </div>
-              <Icons />
-            </Header>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                flexGrow: 1,
-              }}
-            >
-              <Main>
-                <Grid>{children}</Grid>
-              </Main>
-              <Footer>
-                <Grid>
-                  <p>
-                    ©
-                    {' '}
-                    {new Date().getFullYear()}
-                    {' '}
-                    Nicole Bunge
-                  </p>
-
-                  <Navigation routes={[routes.PRIVACY, routes.IMPRINT]} />
-                </Grid>
-              </Footer>
-            </div>
+                <p>
+                  ©
+                  {' '}
+                  {new Date().getFullYear()}
+                  {' '}
+                  Nicole Bunge
+                </p>
+              </Grid>
+            </Footer>
             <Cookie />
+            <Social />
           </div>
         )}
       />
