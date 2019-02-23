b2dea214b648107f72842e0f77749b43c3f88c2c
Marco Biedermann <marco.biedermann@gmx.de>
2019-02-23 11:51:52 +0100

    chore: Sentry
    add Sentry error tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,14 @@ module.exports = {
       resolve: 'gatsby-plugin-react-helmet',
     },
     {
+      resolve: 'gatsby-plugin-sentry',
+      options: {
+        dsn: 'https://0e535dcfe7544f5f9bcbe979d0a5f181@sentry.io/1400930',
+        environment: process.env.NODE_ENV,
+        enabled: (() => ['production', 'stage'].indexOf(process.env.NODE_ENV) !== -1)(),
+      },
+    },
+    {
       resolve: 'gatsby-plugin-sharp',
     },
     {
