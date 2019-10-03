f25359f3ea0eade14b1769f4245cb5b252447139
Marco Biedermann <marco.biedermann@gmx.de>
2019-10-03 22:04:31 +0000

    refactor: styles
    Move global styles into own folder and source in browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,14 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import './src/styles/address.css';
+import './src/styles/all.css';
+import './src/styles/anchor.css';
+import './src/styles/body.css';
+import './src/styles/figure.css';
+import './src/styles/form.css';
+import './src/styles/headline.css';
+import './src/styles/html.css';
+import './src/styles/image.css';
+import './src/styles/list.css';
+import './src/styles/paragraph.css';
+import './src/styles/root.css';
+import './src/styles/selection.css';
 
-// You can delete this file if you're not using it
