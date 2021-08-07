00febd920feeca0cb6b3ca227e822fff7346608e
Marco Biedermann <marco.biedermann@gmx.de>
2021-08-07 20:44:09 +0200

    refactor: Design Tokens
    Use Style Dictionary to generate design tokens

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 import '@fontsource/source-sans-pro/latin-400.css';
 import '@fontsource/source-sans-pro/latin-600.css';
+import './build/css/variables.css';
 import './src/styles/all.css';
 import './src/styles/anchor.css';
 import './src/styles/body.css';
@@ -9,6 +10,5 @@ import './src/styles/html.css';
 import './src/styles/image.css';
 import './src/styles/list.css';
 import './src/styles/paragraph.css';
-import './src/styles/root.css';
 import './src/styles/selection.css';
 import './src/styles/svg.css';
