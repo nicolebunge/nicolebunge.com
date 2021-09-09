b1529f0127386b21c76fb195916f8d1631ce3383
Marco Biedermann <marco.biedermann@gmx.de>
2021-09-09 15:04:25 +0200

    refactor: Config
    Refactor Gatsby Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,15 @@
 /* eslint-disable camelcase */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
+const environment = process.env.NODE_ENV;
+
 module.exports = {
   siteMetadata: {
-    title: 'Nicole Bunge',
+    author: '@BungeNicole',
     description:
       'Schauspielerin & Theaterpädagogin. Schauspielerin am Berliner Kriminal Theater, sowie am Theater aus dem Koffer. Dozentin an der Reduta Schauspielschule Berlin.',
-    author: '@BungeNicole',
     siteUrl: 'https://nicolebunge.com',
+    title: 'Nicole Bunge',
   },
   plugins: [
     {
@@ -19,13 +21,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
+        background_color: '#fff',
+        display: 'minimal-ui',
+        icon: 'src/images/nicole-bunge-icon.png',
         name: 'Nicole Bunge',
         short_name: 'Nicole Bunge',
         start_url: '/',
-        background_color: '#fff',
         theme_color: '#000',
-        display: 'minimal-ui',
-        icon: 'src/images/nicole-bunge-icon.png', // This path is relative to the root of the site.
       },
     },
     {
@@ -35,8 +37,8 @@ module.exports = {
       resolve: 'gatsby-plugin-sentry',
       options: {
         dsn: 'https://0e535dcfe7544f5f9bcbe979d0a5f181@sentry.io/1400930',
-        environment: process.env.NODE_ENV,
-        enabled: (() => ['production', 'stage'].indexOf(process.env.NODE_ENV) !== -1)(),
+        environment,
+        enabled: ['production', 'stage'].includes(environment),
       },
     },
     {
