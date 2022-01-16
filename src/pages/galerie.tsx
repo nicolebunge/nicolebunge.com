07ff44e33d25a8cf3d345b7d21793e326340697a
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-01-16 19:09:37 +0100

    fix: Accessibility Issues
    Fix accessibility issues

--- src/pages/galerie.tsx
+++ src/pages/galerie.tsx
@@ -17,26 +17,38 @@ function GalleryPage(): JSX.Element {
               {
                 src: 'gallery/alice.jpg',
                 alt: 'Rolle: Alice',
+                width: 336,
+                height: 224,
               },
               {
                 src: 'gallery/berliner.jpg',
                 alt: 'Rolle: Berliner',
+                width: 459,
+                height: 573,
               },
               {
                 src: 'gallery/goth.jpg',
                 alt: 'Rolle: Goth',
+                width: 336,
+                height: 420,
               },
               {
                 src: 'gallery/kompagnie-1.jpg',
                 alt: 'Theater Kompagnie Stuttgart',
+                width: 213,
+                height: 213,
               },
               {
                 src: 'gallery/kompagnie-2.jpg',
                 alt: 'Theater Kompagnie Stuttgart',
+                width: 581,
+                height: 465,
               },
               {
                 src: 'gallery/das-tote-maedchen.jpg',
                 alt: 'Rolle: Das tote Mädchen',
+                width: 459,
+                height: 573,
               },
             ]}
           />
---
+++ src/utils/__tests__/string.test.ts
@@ -0,0 +1,11 @@
+import { isExternalUrl } from '../string';
+
+describe('isExternalUrl', () => {
+  it('should check if URL is external', () => {
+    expect.assertions(3);
+
+    expect(isExternalUrl('/about')).toStrictEqual(false);
+    expect(isExternalUrl('http://example.com')).toStrictEqual(true);
+    expect(isExternalUrl('https://example.com')).toStrictEqual(true);
+  });
+});
