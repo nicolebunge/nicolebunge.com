b34f45c8ced2f62a00164bceb2d398f828b0d57b
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2023-07-05 13:43:41 +0200

    chore: SVGr
    Upgrade SVGr

--- src/components/Icons/Xing4.tsx
+++ src/components/Icons/Xing4.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
-import { SVGProps } from 'react';
-
+import type { SVGProps } from 'react';
 const SvgXing4 = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
     <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zM6.089 14.996l2.4-4.233-1.614-2.767h2.881l1.601 2.759-2.401 4.241H6.089zM16.939 19h-2.848l-3.105-5.639L15.694 5h2.877l-4.707 8.361L16.939 19z" />
   </svg>
 );
-
 export default SvgXing4;
