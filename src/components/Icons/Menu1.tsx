654b481714435963ccac2cc4bf93a75a59531143
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2024-08-12 13:42:25 +0200

    chore: CI Workflow
    Adjust CI workflow

--- src/components/Icons/Menu1.tsx
+++ src/components/Icons/Menu1.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgMenu1 = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
-    <path d="M24 6H0V2h24v4zm0 4H0v4h24v-4zm0 8H0v4h24v-4z" />
+    <path d="M24 6H0V2h24zm0 4H0v4h24zm0 8H0v4h24z" />
   </svg>
 );
 export default SvgMenu1;
