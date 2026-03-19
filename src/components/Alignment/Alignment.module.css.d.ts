2eeb11d071126b65b1c526b3b809bd8e6eeddd39
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2026-03-19 15:08:25 +0100

    fix: CSS Modules Modifier
    Fix CSS Modules modifier name imports in alignment component

--- src/components/Alignment/Alignment.module.css.d.ts
+++ src/components/Alignment/Alignment.module.css.d.ts
@@ -1,11 +1,11 @@
 declare const styles: {
   readonly alignment: string;
-  readonly 'alignment-align-center': string;
-  readonly 'alignment-align-end': string;
-  readonly 'alignment-align-start': string;
-  readonly 'alignment-justify-center': string;
-  readonly 'alignment-justify-end': string;
-  readonly 'alignment-justify-space-between': string;
-  readonly 'alignment-justify-start': string;
+  readonly 'alignment--align-center': string;
+  readonly 'alignment--align-end': string;
+  readonly 'alignment--align-start': string;
+  readonly 'alignment--justify-center': string;
+  readonly 'alignment--justify-end': string;
+  readonly 'alignment--justify-space-between': string;
+  readonly 'alignment--justify-start': string;
 };
 export = styles;
