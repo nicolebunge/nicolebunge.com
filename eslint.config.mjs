cdd00a4fa0ad788ec74127bb94e3da3fb05eae0e
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2026-09-06 13:25:31 +0200

    fix: ESLint `eslint-plugin-react`
    Fix for ESLint 10+: eslint-plugin-react uses context.getFilename() (legacy API)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-  ]),
+  globalIgnores(['.next/**', 'build/**', 'coverage/**', 'next-env.d.ts', 'out/**']),
+  {
+    settings: {
+      react: {
+        version: '19',
+      },
+    },
+  },
 ]);
 
 export default eslintConfig;
